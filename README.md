09db4baf1e55d0cf591477faea2db5ed8b0c6f94
Chris McKenzie <christophermckenzie.84@gmail.com>
2023-09-22 18:33:56 -0400

    demo starter

---
+++ README.md
@@ -0,0 +1,9 @@
+# ChromaDB Demo
+
+## Install
+```bash
+git clone git@github.com:kenzic/chromadb-demo.git
+cd chromadb-demo
+git fetch --all --tags
+git checkout tags/basic-demo -b sandbox
+```
---
+++ src/data.js
@@ -0,0 +1,93 @@
+export default [
+    {
+        "id": "1",
+        "title": "Favorable Weather Forecast for Sept. 24 Sample Return",
+        "url": "https://blogs.nasa.gov/osiris-rex/2023/09/23/favorable-weather-forecast-for-sept-24-sample-return/",
+
+        "document": `
+        weather briefing today predicted a dry Sept. 24 with low winds. These are optimal conditions for NASA’s OSIRIS-REx sample recovery team: A wet and windy day would have made a speedy capsule recovery from the desert floor of the Department of Defense’s Utah Test and Training Range more difficult.
+
+        The OSIRIS-REx team will continue monitoring the weather on Sunday via balloons, the first to be released at 5 a.m. EDT (3 a.m. MDT), which will soar to around 60,000 feet above the military range to measure local temperature, humidity, air pressure, and wind.
+
+        A landscape scene with green shrubs in the foreground and a mountain range stretching horizontally across the top half of the image. In the middle of the mounds of shrubs is flat, sand-colored ground.
+
+        This morning, team members packed their supplies for the field and loaded their gear onto the helicopters and vehicles they’ll use Sunday morning to travel to the capsule’s landing location.
+
+        NASA’s live coverage of the OSIRIS-REx capsule landing starts at 10 a.m. EDT (8 a.m. MDT) on Sunday, Sept. 24, and will air on NASA TV, the NASA app, and the agency’s website.
+        `,
+    },
+
+    {
+        "id": "2",
+        "title": "NASA’s Psyche Moves Toward Liftoff",
+        "url": "https://blogs.nasa.gov/psyche/2023/09/22/nasas-psyche-moves-toward-liftoff/",
+        "document": `
+        Now that fueling and testing are complete, NASA’s Psyche spacecraft is ready to meet its ride – a SpaceX Falcon Heavy rocket. Launch is now targeting 10:34 a.m. EDT Thursday, Oct. 5 from Launch Complex 39A at NASA’s Kennedy Space Center in Florida after optimizing the trajectory for the mission to study a metal-rich asteroid.
+
+        Technicians connected Psyche to the payload attach fitting at Astrotech Space Operations facility in Titusville, Florida. This hardware allows Psyche to connect to the top of the rocket once it’s secure inside the protective payload fairings.
+
+        Psyche’s journey through space will last nearly six years and about 2.2 billion miles (3.6 billion kilometers) before reaching an asteroid of the same name, which is orbiting the Sun between Mars and Jupiter. Scientists believe Psyche could be part of the core of a planetesimal, likely made of iron-nickel metal. The ore will not be mined but studied from orbit in hopes of giving researchers a better idea of what may make up Earth’s core.
+
+        Additionally, the Psyche spacecraft will host a pioneering technology demonstration: NASA’s DSOC (Deep Space Optical Communications) experiment. This laser communications system will test high-bandwidth optical communications to Earth for the first two years of Psyche’s journey.
+        `,
+    },
+
+    {
+        "id": "3",
+        "title": "Space Botany, Station Upkeep and Departure Prep for Crew on Friday",
+        "url": "https://blogs.nasa.gov/spacestation/2023/09/22/space-botany-station-upkeep-and-departure-prep-for-crew-on-friday/",
+        "document": `
+        Friday sees a busy day for the Expedition 69 crew ahead of their off-duty weekend aboard the International Space Station. Preparing for upcoming crew departures and October spacewalks, health exams, and space gardening topped the ten crew members’ research schedules today.
+
+        Flight Engineer Jasmin Moghbeli of NASA started her day with orbital plumbing and maintenance on the EXPRESS racks, payloads used for storing research experiments. After lunch, she continued station upkeep tasks, removing and troubleshooting lights and inspecting the station’s cupola, or “window to the world.” Later in the day, Moghbeli was joined once again by ESA (European Space Agency) astronaut Andreas Mogensen and JAXA (Japan Aerospace Exploration Agency) Flight Engineer Satoshi Furukawa for another round of eye exams.
+
+        Mogensen then moved into some space gardening, harvesting the final round of Arabidopsis plants, part of the Plant Habitat-03 investigation. The experiment aims to help researchers better understand how adaptations of plants in one generation could transfer to the next, given the environmental stress of the microgravity environment. Research as such provides an array of scientific data that can be applied on future space missions.
+
+        Ahead of eye exams, Furukawa spent his morning in the Kibo Laboratory Module, installing solid combustion into the Multipurpose Payload Rack, research of this magnitude helping improve fuel efficiency and fire safety both on orbit and on Earth.
+
+        Flight Engineer Loral O’Hara of NASA was joined by Mogensen and NASA astronaut Frank Rubio in the morning to replace batteries and install restraint straps and helmet lights to spacesuits in preparation for a round of U.S. spacewalks in October.
+
+        After spacesuit maintenance, Rubio was joined by Commander Sergey Prokopyev and Flight Engineer Dmitiri Petelin of Roscosmos to train on descent of the Soyuz spacecraft they’ll take home in just a few short days. After spending over a year in space, the three long-serving residents will undock from the station’s Prichal Module at 3:55 a.m. EDT on Wednesday, Sept. 27. Following a quick ride back home, they will land in Kazakhstan at 7:17 a.m.
+
+        The three other crew members of Roscosmos split up orbital duties today. Flight Engineer Konstantin Borisov conducted maintenance activities in the Nauka module, while Oleg Kononenko donned a sensor-packed cap to practice piloting techniques and explore how spacecraft can be controlled on future planetary missions as part of the ongoing Pilot-T investigation. Powered on earlier this week, the EarthKam—a digital camera mounted on the station that is remotely controlled by students to take photographs of Earth—was shut off and stowed for future use by Flight Engineer Nikolai Chub.
+        `
+    },
+    {
+        "id": "4",
+        "title": "Health Activities, Microbial Research for Orbital Residents Today; Crew Handovers Continue",
+        "url": "https://blogs.nasa.gov/spacestation/2023/09/20/health-activities-microbial-research-for-orbital-residents-today-crew-handovers-continue/",
+        "document": `
+        Ten orbital residents are keeping busy Wednesday with a variety of tasks including eye exams, station maintenance, and training. While new members of the Expedition 69 crew familiarize themselves with hardware and equipment, others are completing more prep work ahead of their departure from the International Space Station next week.
+
+        After donning the Sleep in Orbit hardware overnight, ESA (European Space Agency) astronaut Andreas Mogensen started his day filling out a questionnaire that helps researchers gain insight into astronauts’ sleep in space and compare that data to sleep on Earth. Midmorning, he was joined by NASA astronauts and first-time station residents, Loral O’Hara and Jasmin Moghbeli, to make adjustments to their spacesuits that will be used on future spacewalks.
+
+        After lunch, Mogensensen and Moghbeli were joined by JAXA (Japan Aerospace Exploration Agency) astronaut Satoshi Furukawa to conduct ultrasound eye exams. Furukawa spent most of his morning collecting surface samples for microbial analysis ahead of the exam. Afterward, he moved on to collecting air samples to continue the microbial research.
+
+        NASA astronaut Frank Rubio spent Wednesday gearing up for his trek home to Earth next week following a record-breaking mission. He spent part of his day working in the Japanese Experiment Module conducting maintenance before moving onto departure prep, including crew handover activities and prepping items that will return home with the crew on the Soyuz MS-23 spacecraft.
+
+        Rubio, as well as Roscosmos Commander Sergey Prokopyev and Flight Engineer Dmitri Petelin, launched to the space station on Sept. 21, 2022, and will undock on Sept. 27 after spending over one year in space. The two cosmonauts joined Rubio in crew handover and departure prep activities today as they prepare for a change in command and continue to train the newly arrived crew.
+
+        Roscosmos Flight Engineers Oleg Kononenko and Nikolai Chub, who arrived to the orbital outpost last Friday along with O’Hara, completed crew orientation early on in the day. In the evening, the two were joined by Mogensen, O’Hara and Prokopyev to familiarize themselves with hardware and equipment throughout various station modules.
+
+        Meanwhile, Flight Engineer Konstantin Borisov of Roscosmos spent most of the day conducting an experiment that studies the glow of Earth’s atmosphere at night in near ultraviolet.
+        `
+    },
+    {
+        "id": "5",
+        "title": "Artemis II Astronauts Successfully Conduct Launch Day Demonstration",
+        "url": "https://blogs.nasa.gov/artemis/2023/09/20/artemis-ii-astronauts-successfully-conduct-launch-day-demonstration/",
+        "document": `
+        The Artemis II crew and teams with NASA’s Exploration Ground Systems Program successfully completed the first in a series of integrated ground system tests at the agency’s Kennedy Space Center in Florida in preparation for their mission around the Moon.
+
+        On Wednesday, NASA astronauts Reid Wiseman, Victor Glover, and Christina Koch, along with CSA (Canadian Space Agency) astronaut Jeremy Hansen, practiced the procedures they will undergo on launch day to prepare for their ride to space.
+
+        The crew awoke at their crew quarters inside Kennedy’s Neil Armstrong Operations and Checkouts building before putting on test versions of the Orion crew survival system spacesuits they will wear on launch day. They then departed in NASA’s new Artemis crew transportation fleet to take them to Launch Pad 39B, traversing the nine-mile journey to the pad. Wiseman and Glover headed over in the first electric vehicle as Koch and Hansen followed them in the second.
+
+        Upon arrival at the pad, the crew headed onto the mobile launcher and proceeded up the tower to the white room inside the crew access arm. From this area, the astronauts will have access to enter and exit the Orion spacecraft – only for this test, there was no Orion or SLS (Space Launch System) rocket.
+
+        “When we walked out that crew access arm, I just had images of all those Apollo launches and shuttle launches that I saw as a kid and it was unreal,” Glover said. “I actually had to stop and just stay in the moment to really let it all sink in.”
+
+        Successful completion of this test ensures both the crew and the ground systems teams at Kennedy are prepared and understand the timeline of their events for launch day.
+        `
+    }
+]
