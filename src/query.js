d324e35f8bd6c98e408ebf88b4e5bbf17043e062
Chris McKenzie <christophermckenzie.84@gmail.com>
2023-09-23 21:18:54 -0400

    finalized demo

--- src/query.js
+++ src/query.js
@@ -1,3 +1,26 @@
-async function main() {}
+import { ChromaClient, OpenAIEmbeddingFunction } from 'chromadb'
+
+const client = new ChromaClient({
+    auth: { // Provide client with auth options
+        provider: "basic", // Tells client to use basic auth
+        credentials: "admin:admin" // Tells client to use the username "admin" and password "admin"
+    }
+});
+
+const embedder = new OpenAIEmbeddingFunction({openai_api_key: "apiKey"})
+
+async function main() {
+  const collection = await client.getCollection({
+    name: "nasaArticles",
+    embeddingFunction: embedder
+  });
+
+  const results = await collection.query({
+    nResults: 2,
+    queryTexts: ["What's happening on the space station?"]
+  });
+
+  console.log(JSON.stringify(results, null, 2));
+}
 
 main();
--- src/upload.js
+++ src/upload.js
@@ -1,3 +1,41 @@
-async function main() {}
+import { ChromaClient, OpenAIEmbeddingFunction } from 'chromadb'
+import data from "./data";
+
+const client = new ChromaClient({
+  auth: { // Provide client with auth options
+    provider: "basic", // Tells client to use basic auth
+    credentials: "admin:admin" // Tells client to use the username "admin" and password "admin"
+  }
+});
+
+const embedder = new OpenAIEmbeddingFunction({openai_api_key: "apiKey"});
+
+async function main() {
+  const collection = await client.getOrCreateCollection({
+    name: "nasaArticles",
+    embeddingFunction: embedder
+  });
+
+  // add the following:
+  const ids = [];
+  const documents = [];
+  const metadatas = [];
+  data.forEach((article) => {
+    ids.push(article.id);
+    documents.push(article.document);
+    metadatas.push({
+      title: article.title,
+      url: article.url
+    });
+  });
+
+  const result = await collection.add({
+    ids,
+    documents,
+    metadatas
+  });
+
+  console.log('result', result);
+}
 
 main();
